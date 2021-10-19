64b2207f4e38910f4fccbf4207ad2fa6d776fce9
nakzyu <nakzyu@gmail.com>
2021-10-19 14:37:06 +0900

    Fix: message text

--- src/components/shareButtons.tsx
+++ src/components/shareButtons.tsx
@@ -19,8 +19,8 @@ export default function ShareButtons({
       window.Kakao.Link.sendDefault({
         objectType: "feed", // 메시지 형식 : 피드 타입
         content: {
-          title: "개성 넘치고 귀여운 인싸 친구, 아이비",
-          description: "나와 잘 맞는 식물 친구는?",
+          title: "운동하는 곰돌이 테스트",
+          description: "나의 곰돌이 찾기",
           imageUrl: typeImgSrc, // 메인으로 보여질 이미지 주소
           link: {
             webUrl: resultUrl,

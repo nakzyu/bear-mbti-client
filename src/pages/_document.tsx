5f8c773736149b3b1acb0cde64d79787399bd48f
nakzyu <nakzyu@gmail.com>
2021-10-21 15:46:23 +0900

    Refactor: Split scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   DocumentContext,
   Head,
 } from "next/document";
+
 import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
@@ -40,10 +41,25 @@ class MyDocument extends Document {
           <meta property="og:type" content="website" />
           <meta property="og:site_name" content="운동하는 곰돌이 테스트" />
           <link rel="preload" href="/images/loading.png" as="image" />
+
           <script
             defer
             src="https://developers.kakao.com/sdk/js/kakao.js"
           ></script>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-MV95RT581Q"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L6NM1NYDS7');
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
